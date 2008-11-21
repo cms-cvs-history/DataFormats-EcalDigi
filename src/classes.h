bfeb726caabeae8e0a683404b6f427d518bcab96
cmsbuild <>
2008-11-21 00:41:56 +0000

    This commit was manufactured by cvs2git to create tag 'DataFormats-EcalDigi- CMSSW_3_0_0_pre3'.
    Sprout from master 2008-11-21 00:41:54 UTC Giovanni Franzoni <sha1-f0139b50f52dbd5ce9f34dbb70c1246671ec185e@cern.ch> 'imput of containers of TCC EE pseudostrip input data'
    Cherrypick from master 2008-11-11 18:00:50 UTC Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2@cern.ch> 'Change from two nested anonymous namespaces to a "struct dictionary" with':
        interface/EcalDigiCollections.h
        src/classes.h
        src/classes_def.xml

--- src/classes.h
+++ src/classes.h
@@ -7,12 +7,10 @@ namespace {
     std::vector<EcalFEMSample> vFEM_;
     std::vector<ESSample> vESSample_;
     std::vector<EcalTriggerPrimitiveSample> vETPS_;
-    std::vector<EcalPseudoStripInputSample> vEPSIS_;
     std::vector<EcalMatacqDigi> vMD_;
 
     edm::SortedCollection<ESDataFrame> vES_;
     edm::SortedCollection<EcalTriggerPrimitiveDigi> vETP_;
-    edm::SortedCollection<EcalPseudoStripInputDigi> vEPSI_;
     edm::SortedCollection<EBSrFlag> vEBSRF_;
     edm::SortedCollection<EESrFlag> vEESRF_;
     edm::SortedCollection<EcalPnDiodeDigi> vEPN_;
@@ -40,7 +38,6 @@ namespace {
 
     edm::Wrapper< edm::SortedCollection<ESDataFrame> > theESw_;
     edm::Wrapper< edm::SortedCollection<EcalTriggerPrimitiveDigi> > theETPw_;
-    edm::Wrapper< edm::SortedCollection<EcalPseudoStripInputDigi> > theEPSIw_;
     edm::Wrapper< edm::SortedCollection<EBSrFlag> > theEBSRFw_;
     edm::Wrapper< edm::SortedCollection<EESrFlag> > theEESRFw_;
     edm::Wrapper< edm::SortedCollection<EcalPnDiodeDigi> > theEPNw_; 
